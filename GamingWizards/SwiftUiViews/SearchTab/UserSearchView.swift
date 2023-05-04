c94a4be95c9d05c48a1295c8ef2fedfc95f9abe8
Tyler Donohue <donohue_tyler@yahoo.com>
2023-05-04 00:00:16 -0600

    added background image.

--- GamingWizards/SwiftUiViews/SearchTab/UserSearchView.swift
+++ GamingWizards/SwiftUiViews/SearchTab/UserSearchView.swift
@@ -11,43 +11,23 @@ struct UserSearchView: View {
     @Environment(\.presentationMode) var presentationMode
     @StateObject private var userSearchViewModel = UserSearchViewModel()
     
-    
     var body: some View {
-        ZStack(alignment: .bottom) {
-//            backgroundImage
-            //            Image("realistic-billboard")
-            //                .resizable()
-            //                .scaledToFill()
-            //                .edgesIgnoringSafeArea(.all)
+        ZStack {
             NavigationStack {
                 searchBar
-//                backgroundImage
-//                List {
-//
-//                    ForEach(userSearchViewModel.filteredGames, id: \.self) { name in
-//                        VStack {
-//                            Text(name)
-//                        }
-//                    }
-//                }
             }
             .navigationTitle("Looking for Group")
-//            .searchable(text: $userSearchViewModel.searchText)
-//            .searchScopes($userSearchViewModel.searchScope) {
-//                ForEach(SearchScope.allCases, id: \.self) { scope in
-//                    Text(scope.rawValue.capitalized)
-//                }
-//            }
         }
+        .background(
+            backgroundImage
+        )
     }
     
     private var backgroundImage: some View {
-        VStack {
             Image("realistic-billboard")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
-        }
     }
     
     private var searchBar: some View {
@@ -56,36 +36,10 @@ struct UserSearchView: View {
             List(userSearchViewModel.filteredGames, id: \.self) { name in
                 Text(name)
             }
+            .listStyle(.plain)
         }
     }
-    
-//    struct SearchBar: View {
-//        @Binding var text: String
-//        var onSearchButtonClicked: () -> Void
-//
-//        var body: some View {
-//            HStack {
-//                TextField("Search", text: $text)
-//                    .padding(.horizontal, 12)
-//                    .padding(.vertical, 8)
-//                    .background(Color(.systemGray6))
-//                    .cornerRadius(8)
-//                    .padding(.horizontal, 8)
-//                
-//                Button(action: {
-//                    self.onSearchButtonClicked()
-//                }) {
-//                    Text("Search")
-//                        .padding(.horizontal, 12)
-//                        .padding(.vertical, 8)
-//                        .background(Color(.systemBlue))
-//                        .cornerRadius(8)
-//                }
-//            }
-//        }
-//    }
 
-    
 }
 
 struct OrderView_Previews: PreviewProvider {
