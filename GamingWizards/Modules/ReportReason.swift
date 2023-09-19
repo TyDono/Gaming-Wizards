244c736b41453a1aeb9889fb5e9d03bb981246d8
Tyler Donohue <donohue_tyler@yahoo.com>
2023-09-19 16:46:35 -0600

    added logic for the report button added some small ui for the report user button.

---
+++ GamingWizards/Modules/ReportReason.swift
@@ -0,0 +1,8 @@
+//
+//  ReportReason.swift
+//  GamingWizards
+//
+//  Created by Tyler Donohue on 9/19/23.
+//
+
+import Foundation
---
+++ GamingWizards/UniversalViews/Create Report User/CreateReportUserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CreateReportUserViewModel.swift
+//  GamingWizards
+//
+//  Created by Tyler Donohue on 9/19/23.
+//
+
+import Foundation
